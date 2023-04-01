f93f1f5ded098f0d26038ad8dfabc55f77bd4e3a
pablof7z <pfer@me.com>
2023-04-01 12:43:16 +0300

    configuration improvements

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('test')
+import { NDKCacheAdapter, NDKEvent } from '@nostr-dev-kit/ndk';
+import Redis from 'ioredis';
+
+export default class RedisAdapter implements NDKCacheAdapter {
+    public redis;
+
+    constructor() {
+        this.redis = new Redis();
+    }
+
+    public async getEvents(): Promise<Set<NDKEvent>> {
+        return new Set<NDKEvent>();
+    }
+}
